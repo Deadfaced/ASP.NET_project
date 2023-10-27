7a938bece5f4ef7748b8d9c376b95c11bdfe37e9
daniel pereira <danielpereira22costa@gmail.com>
2023-10-27 11:07:45 +0100

    Project fixed

--- MvcBlog/Controllers/HomeController.cs
+++ MvcBlog/Controllers/HomeController.cs
@@ -19,9 +19,21 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(
+            new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
+        );
     }
 }
