937f00c24fd1b89d8ef89766a496f367106e8c00
daniel pereira <danielpereira22costa@gmail.com>
2023-11-03 16:59:27 +0000

    Login system Done, Roles Done, and a shit ton of other shit

--- MvcBlog/Controllers/HomeController.cs
+++ MvcBlog/Controllers/HomeController.cs
@@ -1,21 +1,27 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MvcBlog.Areas.Identity.Data;
 using MvcBlog.Models;
 using MvcBlog.Models.ViewModels;
+
 
 namespace MvcBlog.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<MvcBlogUser> _userManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, UserManager<MvcBlogUser> userManager)
     {
         _logger = logger;
+        this._userManager = userManager;
     }
 
     public IActionResult Index()
     {
+        ViewData["UserID"] = _userManager.GetUserId(this.User);
         return View();
     }
 
--- MvcBlog/Program.cs
+++ MvcBlog/Program.cs
@@ -1,27 +1,97 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MvcBlog.Areas.Identity.Data;
+using MvcBlog.Data;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static async Task Main(string[] args)
+    {
+        // Create a new web application builder with the provided command-line arguments
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Get the connection string from the configuration
+        var connectionString =
+            builder.Configuration.GetConnectionString("JapanWandererContext")
+            ?? throw new InvalidOperationException(
+                "Connection string 'JapanWandererContext' not found."
+            );
+
+        // Add the DbContext to the service collection
+        builder.Services.AddDbContext<MvcBlogContext>(
+            options => options.UseSqlite(connectionString)
+        );
+
+        // Add the default identity system configuration to the service collection
+        builder.Services
+            .AddDefaultIdentity<MvcBlogUser>(
+                options => options.SignIn.RequireConfirmedAccount = false
+            )
+            .AddRoles<IdentityRole>() // Add roles support
+            .AddEntityFrameworkStores<MvcBlogContext>(); // Add EF support
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddRazorPages();
+
+        // Build the application
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+        app.MapRazorPages();
+
+        // Create a new scope to be able to use scoped services
+        using (var scope = app.Services.CreateScope())
+        {
+            // Get the RoleManager service
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var roles = new[] { "Admin", "User"};
+
+            // Ensure the roles exist
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+
+        // Create a new scope to be able to use scoped services
+        using (var scope = app.Services.CreateScope())
+        {
+            // Get the UserManager service
+            var UserManager = scope.ServiceProvider.GetRequiredService<UserManager<MvcBlogUser>>();
+
+            string email = "admin@admin.com";
+            string password = "Admin123!";
+
+            // Ensure the admin user exists
+            if (await UserManager.FindByEmailAsync(email) == null)
+            {
+                var user = new MvcBlogUser { UserName = email, Email = email, FirstName = "Admin", LastName = "Admin"};
+
+                await UserManager.CreateAsync(user, password);
+
+                await UserManager.AddToRoleAsync(user, "Admin");
+            }
+        }
+
+        // Run the application
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
