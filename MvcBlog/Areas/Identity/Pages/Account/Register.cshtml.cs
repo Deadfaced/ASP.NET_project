7de0e2c0caafaefe2c54ff41c6a97166b256abfb
Bernardo Teixeira <btteixeira.bt@gmail.com>
2023-11-06 23:08:38 +0000

    fixed issue with login and register pages

--- MvcBlog/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MvcBlog/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -132,14 +132,14 @@ namespace MvcBlog.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
-                        if (ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 var user = new MvcBlogUser { UserName = Input.Email, Email = Input.Email };
 
                 user.FirstName = Input.FirstName;
                 user.LastName = Input.LastName;
 
-                                var result = await _userManager.CreateAsync(user, Input.Password);
+                var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
                 {
